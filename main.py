3c65a110bd35191d801060db18c57028adeb23a8
Matteo Barbieri <neopard@gmail.com>
2023-03-16 10:04:37 +0100

    Change colormap to grayscale
    Also reformat using black

--- main.py
+++ main.py
@@ -10,22 +10,19 @@ OFF = 0
 vals = [ON, OFF]
 
 
-class Animation():
-
-    def __init__(
-        self,
-        fig, update, fargs,
-        frames,
-        interval,
-        save_count):
+class Animation:
+    def __init__(self, fig, update, fargs, frames, interval, save_count):
 
         self.ani = animation.FuncAnimation(
-            fig, update, fargs=fargs,
+            fig,
+            update,
+            fargs=fargs,
             frames=frames,
             interval=interval,
-            save_count=save_count)
+            save_count=save_count,
+        )
 
-        fig.canvas.mpl_connect('button_press_event', self.toggle_pause)
+        fig.canvas.mpl_connect("button_press_event", self.toggle_pause)
 
         self.paused = False
 
@@ -40,21 +37,21 @@ class Animation():
 def randomGrid(N):
 
     """returns a grid of NxN random values"""
-    return np.random.choice(vals, N*N, p=[0.2, 0.8]).reshape(N, N)
+    return np.random.choice(vals, N * N, p=[0.2, 0.8]).reshape(N, N)
+
 
 def addGlider(i, j, grid):
 
     """adds a glider with top left cell at (i, j)"""
-    glider = np.array([[0, 0, 255],
-                    [255, 0, 255],
-                    [0, 255, 255]])
-    grid[i:i+3, j:j+3] = glider
+    glider = np.array([[0, 0, 255], [255, 0, 255], [0, 255, 255]])
+    grid[i : i + 3, j : j + 3] = glider
+
 
 def addGosperGliderGun(i, j, grid):
 
     """adds a Gosper Glider Gun with top left
     cell at (i, j)"""
-    gun = np.zeros(11*38).reshape(11, 38)
+    gun = np.zeros(11 * 38).reshape(11, 38)
 
     gun[5][1] = gun[5][2] = 255
     gun[6][1] = gun[6][2] = 255
@@ -78,7 +75,8 @@ def addGosperGliderGun(i, j, grid):
     gun[3][35] = gun[3][36] = 255
     gun[4][35] = gun[4][36] = 255
 
-    grid[i:i+11, j:j+38] = gun
+    grid[i : i + 11, j : j + 38] = gun
+
 
 def update(frameNum, img, grid, N):
 
@@ -91,10 +89,19 @@ def update(frameNum, img, grid, N):
             # compute 8-neighbor sum
             # using toroidal boundary conditions - x and y wrap around
             # so that the simulation takes place on a toroidal surface.
-            total = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] +
-                        grid[(i-1)%N, j] + grid[(i+1)%N, j] +
-                        grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] +
-                        grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])/255)
+            total = int(
+                (
+                    grid[i, (j - 1) % N]
+                    + grid[i, (j + 1) % N]
+                    + grid[(i - 1) % N, j]
+                    + grid[(i + 1) % N, j]
+                    + grid[(i - 1) % N, (j - 1) % N]
+                    + grid[(i - 1) % N, (j + 1) % N]
+                    + grid[(i + 1) % N, (j - 1) % N]
+                    + grid[(i + 1) % N, (j + 1) % N]
+                )
+                / 255
+            )
 
             # apply Conway's rules
             if grid[i, j] == ON:
@@ -107,7 +114,8 @@ def update(frameNum, img, grid, N):
     # update data
     img.set_data(newGrid)
     grid[:] = newGrid[:]
-    return img,
+    return (img,)
+
 
 # main() function
 def main():
@@ -115,14 +123,16 @@ def main():
     # Command line args are in sys.argv[1], sys.argv[2] ..
     # sys.argv[0] is the script name itself and can be ignored
     # parse arguments
-    parser = argparse.ArgumentParser(description="Runs Conway's Game of Life simulation.")
+    parser = argparse.ArgumentParser(
+        description="Runs Conway's Game of Life simulation."
+    )
 
     # add arguments
-    parser.add_argument('--grid-size', dest='N', required=False)
-    parser.add_argument('--mov-file', dest='movfile', required=False)
-    parser.add_argument('--interval', dest='interval', required=False)
-    parser.add_argument('--glider', action='store_true', required=False)
-    parser.add_argument('--gosper', action='store_true', required=False)
+    parser.add_argument("--grid-size", dest="N", required=False)
+    parser.add_argument("--mov-file", dest="movfile", required=False)
+    parser.add_argument("--interval", dest="interval", required=False)
+    parser.add_argument("--glider", action="store_true", required=False)
+    parser.add_argument("--gosper", action="store_true", required=False)
     args = parser.parse_args()
 
     # set grid size
@@ -140,36 +150,44 @@ def main():
 
     # check if "glider" demo flag is specified
     if args.glider:
-        grid = np.zeros(N*N).reshape(N, N)
+        grid = np.zeros(N * N).reshape(N, N)
         addGlider(1, 1, grid)
     elif args.gosper:
-        grid = np.zeros(N*N).reshape(N, N)
+        grid = np.zeros(N * N).reshape(N, N)
         addGosperGliderGun(10, 10, grid)
 
-    else: # populate grid with random on/off -
-            # more off than on
+    else:  # populate grid with random on/off -
+        # more off than on
         grid = randomGrid(N)
 
     # set up animation
     fig, ax = plt.subplots()
-    plt.set_cmap('gray')
+    plt.set_cmap("gray")
+    ax.set_axis_off()
 
-    img = ax.imshow(grid, interpolation='nearest')
+    img = ax.imshow(grid, interpolation="nearest")
 
     ani = Animation(
-        fig, update, fargs=(img, grid, N, ),
-        frames = 10,
+        fig,
+        update,
+        fargs=(
+            img,
+            grid,
+            N,
+        ),
+        frames=10,
         interval=updateInterval,
-        save_count=50)
+        save_count=50,
+    )
 
     # # of frames?
     # set output file
     # if args.movfile:
-        # ani.save(args.movfile, fps=30, extra_args=['-vcodec', 'libx264'])
+    # ani.save(args.movfile, fps=30, extra_args=['-vcodec', 'libx264'])
 
     plt.show()
 
-# call main
-if __name__ == '__main__':
-    main()
 
+# call main
+if __name__ == "__main__":
+    main()
